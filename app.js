2f83fd1181550384d690b1431aad14e093f8821d
malacridaanna <malacridaanna@motrust.com>
2023-02-10 12:36:35 +0600

    file

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+
+app.get("/", (req, res) => {
+    res.send("Home Page");
+})
+
+app.get("/user/get", (req, res) => {
+    res.json([
+        {name : "kaosar", age : 25},
+        {name : "ahamed", age : 26}
+    ]);
+});
+
+
+app.post("/user/create", (req, res) => {
+    res.send("user create sucessfull");
+})
+
+app.listen(port, () => {
+    console.log(`app is runnit at http://localhost:${port}`)
+});
+
+
